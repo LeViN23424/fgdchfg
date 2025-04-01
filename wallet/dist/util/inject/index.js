b1132b10a28c5617deaddff5a37a6408179ee369
LeViN23424 <122550956+LeViN23424@users.noreply.github.com>
2025-04-01 07:04:11 +0300

    Add files via upload

---
+++ wallet/dist/util/inject/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.replace = exports.inject = void 0;
+const inject_1 = require("./inject");
+Object.defineProperty(exports, "inject", { enumerable: true, get: function () { return inject_1.inject; } });
+const replace_1 = require("./replace");
+Object.defineProperty(exports, "replace", { enumerable: true, get: function () { return replace_1.replace; } });
