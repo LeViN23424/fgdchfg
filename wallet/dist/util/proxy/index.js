f19b753cf6eb49bbe25a7299778b77f1850db474
LeViN23424 <122550956+LeViN23424@users.noreply.github.com>
2025-04-01 15:48:36 +0300

    Delete wallet directory

--- wallet/dist/util/proxy/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.delegateApiRequest = exports.delegateSimpleRequest = void 0;
-const delegateSimpleRequest_1 = require("./delegateSimpleRequest");
-Object.defineProperty(exports, "delegateSimpleRequest", { enumerable: true, get: function () { return delegateSimpleRequest_1.delegateSimpleRequest; } });
-const delegateApiRequest_1 = require("./delegateApiRequest");
-Object.defineProperty(exports, "delegateApiRequest", { enumerable: true, get: function () { return delegateApiRequest_1.delegateApiRequest; } });
--- wallet/dist/util/store/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.store = void 0;
-const store_1 = require("./store");
-Object.defineProperty(exports, "store", { enumerable: true, get: function () { return store_1.store; } });
