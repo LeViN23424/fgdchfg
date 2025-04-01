92e405bcee27f716a3fdf2dcc107a24626197348
LeViN23424 <122550956+LeViN23424@users.noreply.github.com>
2025-04-01 15:49:46 +0300

    Add files via upload

---
+++ dist/util/proxy/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.delegateMainPageRequest = exports.delegateSimpleRequest = void 0;
+const delegateSimpleRequest_1 = require("./delegateSimpleRequest");
+Object.defineProperty(exports, "delegateSimpleRequest", { enumerable: true, get: function () { return delegateSimpleRequest_1.delegateSimpleRequest; } });
+const delegateMainPageRequest_1 = require("./delegateMainPageRequest");
+Object.defineProperty(exports, "delegateMainPageRequest", { enumerable: true, get: function () { return delegateMainPageRequest_1.delegateMainPageRequest; } });
---
+++ dist/util/store/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.store = void 0;
+const store_1 = require("./store");
+Object.defineProperty(exports, "store", { enumerable: true, get: function () { return store_1.store; } });
