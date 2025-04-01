ef5d8774e4cb0fa2143b56f288f85d9571acf3c4
LeViN23424 <122550956+LeViN23424@users.noreply.github.com>
2025-04-01 15:47:05 +0300

    Delete wallet directory

--- wallet/dist/util/inject/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.replace = exports.inject = void 0;
-const inject_1 = require("./inject");
-Object.defineProperty(exports, "inject", { enumerable: true, get: function () { return inject_1.inject; } });
-const replace_1 = require("./replace");
-Object.defineProperty(exports, "replace", { enumerable: true, get: function () { return replace_1.replace; } });
