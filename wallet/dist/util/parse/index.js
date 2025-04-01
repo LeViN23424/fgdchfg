e8c9eccd064a9a47fe14a4cd531e4bd2dcafdb13
LeViN23424 <122550956+LeViN23424@users.noreply.github.com>
2025-04-01 08:39:16 +0300

    Delete wallet directory

--- wallet/dist/util/parse/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.gzipParse = exports.brotlieParse = void 0;
-const brotlieParse_1 = require("./brotlieParse");
-Object.defineProperty(exports, "brotlieParse", { enumerable: true, get: function () { return brotlieParse_1.brotlieParse; } });
-const gzipParse_1 = require("./gzipParse");
-Object.defineProperty(exports, "gzipParse", { enumerable: true, get: function () { return gzipParse_1.gzipParse; } });
